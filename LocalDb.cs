df778be5acdc4b617afb321d9040de799128b409
pworthy99 <64188940+pworthy99@users.noreply.github.com>
2020-04-29 14:56:06 +0100

    Initial Upload

---
+++ LocalDb.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+
+namespace SQL.LocalDB.Test
+{
+    /// <summary>
+    /// Represents a local database using SQL LocalDB that must be manually created/deleted.
+    /// </summary>
+    public class LocalDb
+    {
+        /// <summary>
+        /// The name of the database represented by this instance of <see cref="LocalDb"/>.
+        /// </summary>
+        private string databaseName;
+        private string dataSource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalDb"/> class.
+        /// </summary>
+        /// <param name="databaseName">The name of the SQL LocalDB database.</param>
+        /// <param name="dataSource">The name of the SQL LocalDB instance to use.</param>
+        public LocalDb(string databaseName, string dataSource = @"(localdb)\v11.0")
+        {
+            this.databaseName = databaseName;
+            this.dataSource = dataSource;
+        }
+
+        /// <summary>
+        /// Gets the connection string to the SQL LocalDB database.
+        /// </summary>
+        public virtual string ConnectionString
+        {
+            get
+            {
+                return this.BuildConnectionString(this.databaseName, this.dataSource);
+            }
+        }
+
+        /// <summary>
+        /// Opens a connection to the SQL LocalDB database.
+        /// </summary>
+        /// <returns>An open connection to the SQL LocalDB database.</returns>
+        public virtual SqlConnection Open()
+        {
+            SqlConnection conn = new SqlConnection(this.ConnectionString);
+            conn.Open();
+
+            return conn;
+        }
+
+        /// <summary>
+        /// Opens a connection to the SQL LocalDB master database.
+        /// </summary>
+        /// <returns>An open connection to the SQL LocalDB master database.</returns>
+        public virtual SqlConnection OpenMaster()
+        {
+            SqlConnection conn = new SqlConnection(this.BuildConnectionString("master", this.dataSource));
+            conn.Open();
+
+            return conn;
+        }
+
+        /// <summary>
+        /// Checks whether the SQL LocalDB database exists or not.
+        /// </summary>
+        /// <returns>True if the database exists, otherwise false.</returns>
+        public virtual bool CheckExists()
+        {
+            using (var conn = this.OpenMaster())
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText =
+                    string.Format(
+                        @"
+IF EXISTS(SELECT * FROM sys.databases WHERE name='{0}')
+BEGIN
+    SELECT 1
+END
+ELSE
+BEGIN
+    SELECT 0
+END",
+                        this.databaseName);
+
+                int result = (int)cmd.ExecuteScalar();
+                bool exists = result == 1;
+
+                return exists;
+            }
+        }
+
+        /// <summary>
+        /// Executes a create database command to create the SQL LocalDB database.
+        /// </summary>
+        public virtual void CreateDatabase()
+        {
+            var sw = Stopwatch.StartNew();
+
+            using (var conn = this.OpenMaster())
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT CONVERT(VARCHAR(255), SERVERPROPERTY('instancedefaultdatapath'))";
+                string defaultDataPath = (string)cmd.ExecuteScalar();
+                cmd.CommandText = "SELECT CONVERT(VARCHAR(255), SERVERPROPERTY('instancedefaultlogpath'))";
+                string defaultLogPath = (string)cmd.ExecuteScalar();
+
+                string filePath = Path.Combine(defaultDataPath, this.databaseName + ".mdf");
+                string logPath = Path.Combine(defaultLogPath, this.databaseName + "_log.ldf");
+
+                cmd.CommandText = string.Format(
+                    @"CREATE DATABASE [{0}]
+        ON PRIMARY (NAME = [{0}], FILENAME = '{1}' )
+        LOG ON (NAME = [{0}_Log], FILENAME = '{2}' )",
+                    this.databaseName,
+                    filePath,
+                    logPath);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            sw.Stop();
+            Console.WriteLine("CreateDatabase completed in {0} ms. ({1})", sw.ElapsedMilliseconds, this.databaseName);
+        }
+
+        /// <summary>
+        /// Executes a drop database command to delete the SQL LocalDB database.
+        /// </summary>
+        public virtual void DeleteDatabase()
+        {
+            var sw = Stopwatch.StartNew();
+
+            using (var conn = this.OpenMaster())
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = string.Format(
+                    @"
+IF EXISTS(SELECT * FROM sys.databases WHERE name='{0}')
+BEGIN
+    ALTER DATABASE [{0}]
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE
+    DROP DATABASE [{0}]
+END",
+                    this.databaseName);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            sw.Stop();
+            Console.WriteLine("DeleteDatabase completed in {0} ms. ({1})", sw.ElapsedMilliseconds, this.databaseName);
+        }
+
+        /// <summary>
+        /// Builds a connection string for the current database, or if a null or empty string is passed in, builds a connection string for the master database.
+        /// </summary>
+        /// <param name="databaseName">The name of the database to connect to. If null or empty string is provided, the connection string will connect to the master database.</param>
+        /// <param name="dataSource">The Sql Server instance to connect to.</param>
+        /// <returns>A connection string</returns>
+        protected virtual string BuildConnectionString(string databaseName, string dataSource)
+        {
+            var sb = new SqlConnectionStringBuilder();
+            sb.DataSource = dataSource;
+
+            if (string.IsNullOrWhiteSpace(databaseName) == false)
+            {
+                sb.InitialCatalog = databaseName;
+            }
+
+            return sb.ConnectionString;
+        }
+    }
+}
---
+++ TempLocalDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL.LocalDB.Test
+{
+    /// <summary>
+    /// Represents a local database using SQL LocalDB that is automatically (re)created each time an instance of this class is created and is deleted when the instance is disposed.
+    /// </summary>
+    public class TempLocalDb : LocalDb, IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TempLocalDb"/> class.
+        /// </summary>
+        /// <param name="databaseName">The name of the SQL LocalDB database.</param>
+        /// <param name="dataSource">The SQL Server instance to connect to, by default v11.0</param>
+        public TempLocalDb(string databaseName, string dataSource = @"(localdb)\v11.0")
+            : base(databaseName, dataSource)
+        {
+            this.DeleteDatabase();
+            this.CreateDatabase();
+        }
+
+        /// <summary>
+        /// Deletes the database.
+        /// </summary>
+        public void Dispose()
+        {
+            this.DeleteDatabase();
+        }
+    }
+}
